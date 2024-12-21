e0fc2f42e5982f70d7d2aa9a603944368ad91780
Efim Klaus <nuclearmarmot@yandex.ru>
2024-12-21 19:55:58 +0300

    add some options to run browsers maximized

--- src/main/java/model/WebDriverFactory.java
+++ src/main/java/model/WebDriverFactory.java
@@ -2,22 +2,24 @@ package model;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WebDriverFactory
-{
-    public WebDriver createForName(String browserName)
-    {
-        if(browserName.equals("Chrome"))
-        {
-            return new ChromeDriver();
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class WebDriverFactory {
+    public WebDriver createForName(String browserName) {
+        if(browserName.equals("Chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            return new ChromeDriver(options);
         }
-        else if(browserName.equals("Firefox"))
-        {
-            return new FirefoxDriver();
+        else if(browserName.equals("Firefox")) {
+            FirefoxDriver a = new FirefoxDriver();
+            a.manage().window().maximize();
+            return a;
         }
-        else
-        {
+        else {
             throw new RuntimeException("Нераспознаный браузер"+browserName);
         }
     }
