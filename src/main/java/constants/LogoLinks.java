ee1a708df43c247cf52b822a4aab964c3839ab76
Efim Klaus <nuclearmarmot@yandex.ru>
2024-12-21 16:51:26 +0300

    Return of the previosly removed contants

---
+++ src/main/java/constants/LogoLinks.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class LogoLinks
+{
+    public static final String SCOOTER_LOGO_LINK = "https://qa-scooter.praktikum-services.ru/";
+    public static final String YANDEX_LOGO_LINK = "https://dzen.ru/?yredirect=true";
+}
